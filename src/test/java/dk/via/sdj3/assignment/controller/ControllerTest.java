83d1dd0552f361245f87f8e01763e3aabe3ca824
Andrejs <ledaviskylv@gmail.com>
2022-10-10 14:19:46 +0200

    Tests

---
+++ src/test/java/dk/via/sdj3/assignment/controller/ControllerTest.java
@@ -0,0 +1,43 @@
+package dk.via.sdj3.assignment.controller;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dk.via.sdj3.assignment.AssignmentApplication;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.IOException;
+
+// source: https://www.tutorialspoint.com/spring_boot/spring_boot_rest_controller_unit_test.htm
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = AssignmentApplication.class)
+@WebAppConfiguration
+public abstract class ControllerTest {
+    protected MockMvc mvc;
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
+    protected void setUp() {
+        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+    protected String mapToJson(Object obj) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules();
+        return objectMapper.writeValueAsString(obj);
+    }
+    protected <T> T mapFromJson(String json, Class<T> clazz)
+            throws JsonParseException, JsonMappingException, IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules();
+        return objectMapper.readValue(json, clazz);
+    }
+}
