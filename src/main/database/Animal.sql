c094be61f550cda15ec1e2547639d6432014ba99
Andrejs U <ledaviskylv@gmail.com>
2022-10-31 15:08:24 +0100

    Merge conflict shenanigans

---
+++ src/main/database/Animal.sql
@@ -0,0 +1,18 @@
+DROP SCHEMA SDJHandin CASCADE;
+CREATE SCHEMA IF NOT EXISTS SDJHandin;
+
+CREATE TABLE IF NOT EXISTS Animal
+(
+    registrationNumber BIGINT PRIMARY KEY,
+);
+
+
+CREATE TABLE IF NOT EXISTS Product(
+                                      id BIGINT PRIMARY KEY,
+);
+
+CREATE TABLE IF NOT EXISTS AnimalInProduct(
+    productId BIGINT REFERENCES Product(id),
+    animalId BIGINT REFERENCES Animal(registrationNumber),
+    PRIMARY KEY (productId, animalId)
+    );
