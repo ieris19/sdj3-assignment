6dd7f2318b1b7749b6db6d288e3841d45a4516bb
Hugo Daniel <82777546+HugoSilva4@users.noreply.github.com>
2022-10-31 21:21:25 +0100

    Updated pom.xml

--- src/main/java/dk/via/sdj3/assignment/AssignmentApplication.java
+++ src/main/java/dk/via/sdj3/assignment/AssignmentApplication.java
@@ -2,7 +2,7 @@ package dk.via.sdj3.assignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.io.FileNotFoundException;
+
 
 @SpringBootApplication
 public class AssignmentApplication {
